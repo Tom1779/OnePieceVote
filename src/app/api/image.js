a01b6bc9c12783bcdecde076f80572a9f89b67f8
Tom1779 <tom.arad.2001@gmail.com>
2024-11-25 04:30:03 -0800

    updated gitignore

--- src/app/api/image.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const imageUrl = decodeURIComponent(req.query.url);
-
-  const response = await fetch(imageUrl);
-  const buffer = await response.buffer();
-
-  res.setHeader("Content-Type", response.headers.get("content-type"));
-  res.send(buffer);
-}
