84129a993ceb382333301c9a6207a4892ab78975
Tom1779 <tom.arad.2001@gmail.com>
2024-11-25 16:23:39 -0800

    fix image expansion

--- src/app/components/ImageModal.js
+++ src/app/components/ImageModal.js
@@ -13,17 +13,15 @@ export default function ImageModal({ src, alt, onClose }) {
       className="fixed inset-0 z-50 flex items-center justify-center bg-black/75 p-4"
       onClick={handleBackdropClick}
     >
-      <div className="relative max-w-[90vw] max-h-[90vh] bg-gray-900 rounded-xl">
-        <div className="relative w-[80vw] h-[80vh] flex items-center justify-center">
-          <Image
-            src={src}
-            alt={alt || "Image"}
-            width={700}
-            height={700}
-            style={{ objectFit: "contain" }}
-            className="rounded-xl"
-          />
-        </div>
+      <div className="relative max-w-[90vw] max-h-[90vh] bg-gray-900 rounded-xl flex items-center justify-center">
+        <Image
+          src={src}
+          alt={alt || "Image"}
+          width={700}
+          height={700}
+          style={{ objectFit: "contain" }}
+          className="rounded-xl"
+        />
         <button
           onClick={onClose}
           className="absolute top-2 right-2 p-2 bg-black/50 hover:bg-black/75 rounded-full transition-all duration-200 transform hover:scale-110 focus:outline-none focus:ring-2 focus:ring-white/50"
