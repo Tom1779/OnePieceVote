5baf7cd53e8389297dec9823b36d86ca5017ae19
Tom1779 <tom.arad.2001@gmail.com>
2024-11-29 03:36:46 -0800

    change image directory path

--- src/app/page.js
+++ src/app/page.js
@@ -199,7 +199,7 @@ export default function Page() {
                         className="flex-shrink-0"
                       >
                         <Image
-                          src={`/characters/${character.name}`}
+                          src={`characters/${character.name}`}
                           alt={character.name}
                           width={700}
                           height={700}
