0244cd074278cddad4905d50324eed332eb503de
Tom1779 <tom.arad.2001@gmail.com>
2025-01-27 17:22:46 -0800

    Added Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { AuthProvider } from "../contexts/auth-context";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "One Piece Character Voting",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
     <html>
       <body>
         <AuthProvider>{children}</AuthProvider>
+        <Analytics></Analytics>
       </body>
     </html>
   );
