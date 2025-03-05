316948f74729c0ee74268d381de51feea5f99508
Tom1779 <tom.arad.2001@gmail.com>
2025-03-05 12:44:57 -0800

    removed preview component and adjusted layout metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,27 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "One Piece Character Voting",
   description: "Vote for your favorite One Piece characters",
+  openGraph: {
+    title: "One Piece Character Voting",
+    description: "Vote for your favorite One Piece characters",
+    url: "https://www.onepiecevoting.com/",
+    siteName: "One Piece Voting",
+    images: [
+      {
+        url: "https://www.onepiecevoting.com/previews/home.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "One Piece Character Voting",
+    description: "Vote for your favorite One Piece characters",
+    images: ["https://www.onepiecevoting.com/previews/home.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
