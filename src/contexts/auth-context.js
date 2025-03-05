e27fca1e1842a2baf8fb784990b0a2b257c7969c
Tom1779 <tom.arad.2001@gmail.com>
2025-03-05 13:10:04 -0800

    fixinf sign in url

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -44,7 +44,7 @@ export function AuthProvider({ children }) {
 
   const signIn = async () => {
     try {
-      const { error } = await supabase.auth.signInWithOAuth({
+      const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
           redirectTo: `${window.location.origin}/auth/callback`,
