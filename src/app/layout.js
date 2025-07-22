121f7ae63f4be08c75d7b0a904b35d90e6527513
Tom1779 <tom.arad.2001@gmail.com>
2025-07-22 15:26:05 -0700

    new logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,22 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "One Piece Character Voting",
   description: "Vote for your favorite One Piece characters",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    other: [
+      {
+        url: "/favicon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  },
   openGraph: {
     title: "One Piece Character Voting",
     description: "Vote for your favorite One Piece characters",
