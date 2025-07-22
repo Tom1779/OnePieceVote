ad841af17ff2b77bc0100376ca1b512565cb3234
Tom1779 <tom.arad.2001@gmail.com>
2025-07-22 15:31:04 -0700

    new logo

--- src/app/page.js
+++ src/app/page.js
@@ -105,7 +105,12 @@ export default function Page() {
           <div className="flex justify-between h-14 sm:h-16 items-center">
             {/* Logo and Title */}
             <div className="flex items-center space-x-2">
-              <Star className="text-yellow-500 h-4 w-4 sm:h-6 sm:w-6" />
+              {/* <Star className="text-yellow-500 h-4 w-4 sm:h-6 sm:w-6" /> */}
+              <img
+                src="/logo.svg"
+                alt="Logo"
+                className="h-4 w-4 sm:h-6 sm:w-6"
+              />
               <div className="text-xs sm:text-xl font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent truncate max-w-[120px] sm:max-w-none">
                 One Piece Character Voting
               </div>
