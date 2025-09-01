be23dd716c0f7de41144d814c69fbab1fa3879a8
Tom1779 <tom.arad.2001@gmail.com>
2025-09-01 15:03:24 -0700

    Added Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,6 +48,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <script async src='https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NEMVW3V3Z1');
+</script>
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
         <SpeedInsights></SpeedInsights>
